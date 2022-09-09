388ceae78f349cfb6379f529bf84823e5018464c
TehGaa <syosuasaputra1010@gmail.com>
2022-09-09 22:40:02 +0700

    Update app katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+context = {
+    "item": CatalogItem.objects.all(),
+    "nama": "Steven Yosua Saputra",
+    "students_id": "2106750780"
+}
 
 # TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html", context)
