76f3ede941567b31082cb5d45af9c15c66728abd
TehGaa <syosuasaputra1010@gmail.com>
2022-09-12 23:15:23 +0700

    updated urls and views

--- katalog/views.py
+++ katalog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from katalog.models import CatalogItem
-context = {
-    "item": CatalogItem.objects.all(),
-    "nama": "Steven Yosua Saputra",
-    "students_id": "2106750780"
-}
+
 
 # TODO: Create your views here.
 def show_katalog(request):
+    context = {
+        "item": CatalogItem.objects.all(),
+        "nama": "Steven Yosua Saputra",
+        "students_id": "2106750780"
+    }
     return render(request, "katalog.html", context)
