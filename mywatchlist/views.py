db9a7f1cbbc7b2c0423ded1fb8e6e8341c43e1c6
TehGaa <syosuasaputra1010@gmail.com>
2022-09-22 01:02:57 +0700

    Pengerjaan Bonus

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -6,11 +6,25 @@ from django.core import serializers
 
 def show_watchlist(request):
     item = WatchlistModel.objects.all()
+
+    n_no, n_yes = 0, 0
+    for i in item:
+        if i.watched == "YES":
+            n_yes += 1
+        else:
+            n_no += 1
+
     context = {
         "nama": "Steven Yosua Saputra",
         "students_id": 2106750780,
         "item": item
     }
+
+    if (n_yes >= n_no):
+        context["selamat"] = "Selamat, kamu sudah banyak menonton!"
+    else:
+        context["selamat"] = "Wah, kamu masih sedikit menonton!"
+
     return render(request, "watchlist_index.html", context = context)
 
 def show_json(request):
