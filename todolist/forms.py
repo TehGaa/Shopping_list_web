96361ffd0c9e161d363d10676b838b2eabc52b69
TehGaa <syosuasaputra1010@gmail.com>
2022-09-29 03:13:33 +0700

    edit forms.py

--- todolist/forms.py
+++ todolist/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.db import models
 
 class TaskForm(forms.Form):
-    judul_task = forms.CharField(max_length = 225)
-    deskripsi = forms.CharField(max_length = 225)
+    judul_task = forms.CharField(max_length = 225, strip = False)
+    deskripsi = forms.CharField(max_length = 225, strip = False)
 
     
